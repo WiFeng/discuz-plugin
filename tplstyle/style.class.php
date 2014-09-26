41d5a2551cfec4442d0c72fd74acead90071a591
WiFeng <admin@521-wf.com>
2014-09-26 18:59:03 +0800

    ~~

--- tplstyle/style.class.php
+++ tplstyle/style.class.php
@@ -9,15 +9,18 @@ if(!defined('IN_DISCUZ')) {
 }
 
 class plugin_tplstyle {
+
     public function common() {
         global $_G;
 
         $data = C::t('#tplstyle#plugin_tplstyle')->fetch_all();
+        $domain = isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+
         foreach ($data as $val) {
             if (!$val['status']) {
                 continue;
             }
-            if ($val['domain'] && $val['domain'] != $_SERVER['SERVER_NAME']) {
+            if ($val['domain'] && strpos($domain, $val['domain']) === false) {
                 continue;
             }
             if ($val['script'] && $val['script'] != CURSCRIPT) {
