f71a7e9e1292102b90de92d584990c1b31cbdaac
WiFeng <admin@521-wf.com>
2014-09-26 18:43:33 +0800

    修复无参数时风格切换BUG

--- tplstyle/style.class.php
+++ tplstyle/style.class.php
@@ -9,18 +9,15 @@ if(!defined('IN_DISCUZ')) {
 }
 
 class plugin_tplstyle {
-
     public function common() {
         global $_G;
 
         $data = C::t('#tplstyle#plugin_tplstyle')->fetch_all();
-        $domain = isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
-
         foreach ($data as $val) {
             if (!$val['status']) {
                 continue;
             }
-            if ($val['domain'] && strpos($domain, $val['domain']) === false) {
+            if ($val['domain'] && $val['domain'] != $_SERVER['SERVER_NAME']) {
                 continue;
             }
             if ($val['script'] && $val['script'] != CURSCRIPT) {
@@ -30,24 +27,32 @@ class plugin_tplstyle {
                 continue;
             }
 
-            if ($_GET && $val['param']) {
+            if ($val['param']) {
+                if (empty($_GET)) {
+                    continue;
+                }
                 $enable = false;
                 $param = json_decode($val['param'], true);
                 foreach ($param as $vv) {
                     parse_str($vv, $pp);
-                    $diff = array_diff($pp, $_GET);
+                    $diff = array_diff_assoc($pp, $_GET);
                     if (!$pp || !$diff) {
                         $enable = true;
                         break;
                     }
                 }
-
                 if (!$enable) {
                     continue;
                 }
             }
+
             $_G['category']['styleid'] = $val['styleid'];
             break;
+        }
+
+        if (defined('DISCUZ_DEBUG') && DISCUZ_DEBUG) {
+            $_G['setting']['cacheindexlife'] = 0;
+            $_G['setting']['cachethreadlife'] = 0;
         }
     }
 }
